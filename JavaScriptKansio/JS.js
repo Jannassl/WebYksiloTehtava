f572b4c02155c94a6baf557e56056655e5ca84f8
Jannassl <Jan.nassling@metropolia.fi>
2024-04-04 10:35:45 +0300

    Updates

--- JavaScriptKansio/JS.js
+++ JavaScriptKansio/JS.js
@@ -8,6 +8,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 
+
 let restaurants = []
 async function getRestaurants() {
     try {
@@ -28,27 +29,47 @@ async function getRestaurants() {
             for(let restaurant of restaurants){
                 let lat = restaurant.location.coordinates[1];
                 let lng = restaurant.location.coordinates[0];
-                L.marker([lat, lng]).addTo(map).bindPopup(`<h3>${restaurant.name}</h3><p>${restaurant.address}</p>`);
+                let marker = L.marker([lat, lng]).addTo(map).bindPopup(`<h3>${restaurant.name}</h3>
+                <p>${restaurant.address} / ${restaurant.postalCode}</p>
+                <p>tel: ${restaurant.phone} </p>
+                <p>id:  ${restaurant._id}</p>
+                `);
+            
+                marker.on('click', async function() {
+                            
+                    const menu = await getDailyMenu(restaurant._id, 'fi'); // Jos ongelmia niin tämä oli 'en' 
+                    document.getElementById('information').innerHTML = `
+                        <h2>${restaurant.name}</h2>
+                        <p>${restaurant.address}</p>
+                        <h3>Menu</h3>
+                        <p>${menu.courses[0].name}</p>
+                    `;
+                });
             }
         }
     } catch (error) {
         console.log("Error: ", error);
     }
 }
-getRestaurants();
+
+
+//getRestaurants();
 
 async function getDailyMenu(id, language){
     try{
         const response = await fetch(`https://10.120.32.94/restaurant/api/v1/restaurants/daily/${id}/${language}`);
-        if(!response.ok) {
+        if (!response.ok) {
             throw new Error("HTTP error, status = " + response.status);
-        } else {
-            const data = await response.json();
-            console.log(data)
         }
-    }catch(error) {
-        console.log("Error: "+ error);
+        const menu = await response.json();
+        console.log(menu);
+        return menu;
+    } catch (error) {
+        console.log("Error: ", error);
     }
 }
 
-getDailyMenu();
+
+
+
+
